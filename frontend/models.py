1be5266e943e2facb02d6d3c4e39e9b0d27d4232
kirmola <kirmolaa@gmail.com>
2025-04-09 08:28:47 +0530

    image sizes

--- frontend/models.py
+++ frontend/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from autoslug import AutoSlugField
 from django.urls import reverse
 # Create your models here.
 
